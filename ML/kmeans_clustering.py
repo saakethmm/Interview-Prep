48d4c72dd016ecc573d0feb542db9ffd4fe639c8
Saaketh Medepalli <saakethm@umich.edu>
2026-02-12 12:34:05 -0800

    updated problems

---
+++ ML/kmeans_clustering.py
@@ -0,0 +1,20 @@
+from typing import List 
+import math
+
+def min_distance_from_cluster(arrs: List, k: int):
+
+	min_distance = math.inf
+
+	# sort list first
+
+	# expectation step (calculate new distances)
+	# TODO: divide into chunks and calculate distance from each point
+	# to new center
+
+	# TODO: based on proximity to each center, assign closer one
+
+	# maximization step (calculate error and new centers)
+	# TODO: average distance from centers, use this as update step
+	# average to get new center
+
+	pass
